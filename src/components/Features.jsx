1ee2130ef9137d9c5d770ec7550e2670e62532b0
mahak-vaid <125487704+mv991@users.noreply.github.com>
2024-07-31 22:30:47 +0530

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -37,13 +37,13 @@ const Features = () => {
     <section className="h-full common-padding bg-zinc relative overflow-hidden">
       <div className="screen-max-wdith">
         <div className="mb-12 w-full">
-          <h1 id="features_title" className="section-heading">
+          <h1 id="features_title" className="section-heading ml-[70px]">
             Explore the full story.
           </h1>
         </div>
 
         <div className="flex flex-col justify-center items-center overflow-hidden">
-          <div className="mt-32 mb-24 pl-24">
+          <div className="mt-32 mb-24 pl-24 sm:mr-auto">
             <h2 className="text-5xl lg:text-7xl font-semibold">iPhone.</h2>
             <h2 className="text-5xl lg:text-7xl font-semibold">
               Forged in titanium.
